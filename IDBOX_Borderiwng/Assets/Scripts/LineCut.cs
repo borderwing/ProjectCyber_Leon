c7d16b2060e90d5142654e2bec2aaac2f57f5f68
Ye Wu <borderwing@sjtu.edu.cn>
2017-02-09 00:07:40 +0800

    2017/2/9 updated

---
+++ IDBOX_Borderiwng/Assets/Scripts/LineCut.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineCut : MonoBehaviour {
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Vector3 CollisionPoint;
+        CollisionPoint = collision.contacts[0].point;
+        print(CollisionPoint);
+        Debug.Log(CollisionPoint);
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        Invoke("OnCollisionEnter2D", 0.00001f);
+        if (other.gameObject.tag == "EnemyLine")
+            Debug.Log("Object caught");
+		    Destroy (other.gameObject);
+	}
+
+}
